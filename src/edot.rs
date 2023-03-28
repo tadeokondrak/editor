d860b34cbba104f407c4e74519dd7e84ffd5ec08
Tadeo Kondrak <me@tadeo.ca>
2023-03-27 22:56:16 -0600

    Allow adding a newline at the end of a file without one

--- src/edot.rs
+++ src/edot.rs
@@ -330,12 +330,18 @@ impl Edot {
                                 )?;
                             }
                             Mode::Append => {
-                                self.move_selection(
+                                match self.move_selection(
                                     self.focused,
                                     selection_id,
                                     Movement::Right(1),
                                     true,
-                                )?;
+                                ) {
+                                    Ok(()) | Err(MovementError::NoNextLine) => {
+                                        // Ignore the error here so we can add a newline at the end of a
+                                        // file even if there isn't one there already.
+                                    }
+                                    Err(e) => return Err(e.into()),
+                                }
                                 self.insert_char_after(self.focused, selection_id, c);
                             }
                             _ => unreachable!(),
